fadf78abb8d9a4fc323a5c40a4f0c962364790fc
Alicia Lau <alicia@Alicias-MacBook-Pro.local>
2024-03-05 10:42:56 -0500

    updated packages

--- resume-portfolio/src/App.js
+++ resume-portfolio/src/App.js
@@ -8,12 +8,13 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/resume-portfolio">
       <Navbar />
       <div className='flex h-screen justify-center items-center font-sans pt-36'>
         <div className='max-w-6xl mx-auto px-4 pt-4'>
           <Routes>
             <Route exact path="/home" element={<LandingPage />} />
+            <Route exact path="/" element={<LandingPage />} />
             <Route path="/about" element={<About />} />
             <Route path="/projects" element={<Projects />} />
             {/* Add other routes here */}
